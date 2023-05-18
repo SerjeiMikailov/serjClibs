6487d64a5677477d0310e1921f442d11a94d11b7
SerjeiMikailov <felipemaders8@gmail.com>
2023-05-18 10:13:35 -0300

    g

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-
 destination_dir="$(pwd)"
 
-git clone <https://github.com/SerjeiMikailov/mathlib.git> "$destination_dir/mylib"
+git clone https://github.com/SerjeiMikailov/mathlib.git "$destination_dir/mylib"
 
-cd "$destination_dir/mylib"
+chmod +x "$destination_dir/mylib/install_mylib.sh"
+
+"$destination_dir/mylib/install_mylib.sh"
 
 echo "A biblioteca mylib foi instalada com sucesso em $destination_dir/mylib"
