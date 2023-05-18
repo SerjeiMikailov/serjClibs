e4becbdac27224958274a06f221da099b15c049a
SerjeiMikailov <felipemaders8@gmail.com>
2023-05-18 10:06:52 -0300

    g

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+destination_dir="$(pwd)"
+
+git clone <URL_DO_REPOSITORIO> "$destination_dir/mylib"
+
+cd "$destination_dir/mylib"
+
+echo "A biblioteca mylib foi instalada com sucesso em $destination_dir/mylib"
